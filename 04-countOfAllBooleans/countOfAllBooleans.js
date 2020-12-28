becba37799bf33f8851b30e6d8da227a6ee25eee
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 01:11:32 -0500

    commit code file

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var totalCount=0;
+  for(var cursor=0;cursor<arr.length;cursor++)
+  {
+    if(arr[cursor]==true){
+      totalCount++;
+    }
+    if(arr[cursor]==false){
+      totalCount++;
+    }
+    return totalCount;
 }
 
 // Do not edit this line;
