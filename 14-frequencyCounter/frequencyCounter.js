59b58a78ce7403be89152cddffb2787d63622c1f
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 09:27:10 -0500

    commit code file

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,16 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var letterFreq={};
+  for(var cursor=0;cursor<word.length;cursor++){
+    var letter=word.charAt(cursor);
+	  if(dict[letter]){
+	    letterFreq[letter]++;
+	  }
+	  else{
+	    letterFreq[letter]=1;
+	  }
+  }
+  return letterFreq;
 }
 
 // Do not edit this line;
