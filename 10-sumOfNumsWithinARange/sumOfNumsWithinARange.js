81279a9065a35fb0bbf6252bce39ba6817dda423
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 01:49:52 -0500

    commit code file

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var totalCount=0;
+  for(var cursor=0;cursor<nums.length;cursor++){
+    if(nums[cursor]>=start && nums[cursor]<=end){
+      totalCount++;
+    }
+  }
+  return totalCount;
 }
 
 // Do not edit this line;
