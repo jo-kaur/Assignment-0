1e429f14f7b69e4cd1bde72e91f4ee5030c9e008
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 02:14:47 -0500

    commit code file

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  var min = nums[0];
+  var max = nums[0];
+  for(var cursor=0;cursor<nums.length;cursor++){
+    if(nums[cursor]>max){
+	    max= nums[cursor];
+    }
+    if(nums[cursor]<min){
+	    min= nums[cursor];
+    }
+  }
+  return min+max;
 }
 
 // Do not edit this line;
