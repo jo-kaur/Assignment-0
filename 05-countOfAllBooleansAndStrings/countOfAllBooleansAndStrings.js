94fa0c0313e133c553f255f18b3943d055f88f28
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 01:23:45 -0500

    commit code file

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var totalCount=arr.length;
+  for(var cursor=0;cursor<arr.length;cursor++){
+    if(typeof arr[cursor]!="boolean" || typeof arr[cursor]!="string"){
+      totalCount--;
+    }
+  }
+  return totalCount;
 }
 
 // Do not edit this line;
