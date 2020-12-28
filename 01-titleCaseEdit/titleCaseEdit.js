8b9137cede9a3bc8d2864dd088de2aa7305a01d2
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 00:50:28 -0500

    commit code file

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  var title = title.toLowerCase().split(" ");
+
+ for (var i = 0; i < title.length; i++){
+   title[i] = title[i][0].toUpperCase() + title[i].slice(1);
+  }
+
+  return title.join(" ");
 }
 
 // Do not edit this line;
