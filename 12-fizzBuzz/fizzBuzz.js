b5facc9177ee72e3fbd45ef2266850aa10ffbff6
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 02:23:48 -0500

    commit code file

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,21 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  var resultArr=[];
+  for(var num=start;num<=end;num++){
+    if(num%15==0){
+	    resultArr.push("FizzBuzz");
+    }
+    else if(num%3==0){
+	    resultArr.push("Fizz");
+    }
+    else if(num%5==0){
+	    resultArr.push("Buzz");
+    }
+    else{
+	    resultArr.push(i);
+    }
+  }
+  return resultArr;
 }
 
 // Do not edit this line;
