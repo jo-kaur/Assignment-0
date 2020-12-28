d490b81837470b3cf77380a21436b7de3488c2ec
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 09:39:57 -0500

    commit code file

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,15 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    var sum=0;
+    if(num>1){
+      sum=sum+num;
+      num--;
+    }
+    else{
+      sum=sum+1;
+    }
+    return sum;
   }
 }
 
