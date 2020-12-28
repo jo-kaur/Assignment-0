6076ebaa17ed7021f522e41c7e4c185ede605d45
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 01:40:00 -0500

    commit code file

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var totalCount=0;
+  if(var cursor=0;cursor<nums.length;cursor++){
+    if((nums[cursor]%2)==0){
+      totalCount++;
+    }
+  }
+  return totalCount;
 }
 
 // Do not edit this line;
