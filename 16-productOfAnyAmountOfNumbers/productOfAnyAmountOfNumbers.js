bd2340dccf1772d7159aca462528fd7fabbf9d75
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 09:53:35 -0500

    commit code file

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var result=1;
+  for(var cursor=0;cursor<args.length;cursor++){
+    result=result*args[cursor];
+  }
+  return result;
 }
 
 // Do not edit this line;
