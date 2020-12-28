3d9b0227bac3dea926d387e17edde24d545c39af
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2020-12-28 01:34:48 -0500

    commit code file

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,12 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var totalCount=0;
+  if(var cursor=0;cursor<nums.length;cursor++){
+    if(cursor==nums[cursor]){
+      totalCount++;
+    }
+  }
+  return totalCount;
 }
 
 // Do not edit this line;
