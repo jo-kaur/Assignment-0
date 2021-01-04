266637cc3c336ddc8e40f95ed8b81cc8d10ef64c
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2021-01-03 23:33:48 -0500

    Commit code file

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  var result= new Set();
+  for(var i of args){
+    for(var j of i){
+      if(result.has(j)==false){
+        result.add(j);
+      }
+    }
+  }
+  return result;
 }
 
 // Do not edit this line;
--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,17 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    var midNum=Math.floor(nums.length/2);
+    if(nums.length==1 && nums[0]!=target){
+      return false;
+    }
+    if (target == nums[midNum]){
+      return true;
+    }else if (target < nums[midNum]){
+      return this.binarySearch(nums.slice(0, midNum), target);
+    }else if (target > nums[midNum]){
+      return this.binarySearch(nums.slice(midNum), target);
+    }
   }
 }
 
